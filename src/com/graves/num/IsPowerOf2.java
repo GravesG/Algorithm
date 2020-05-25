2923e9aa15408f658a29af32a64d3b730bb4f574
GravesG <469923834@qq.com>
2020-05-25 21:48:07 +0800

    是否是2的整数幂 - O(1)

---
+++ src/com/graves/num/IsPowerOf2.java
@@ -0,0 +1,21 @@
+package com.graves.num;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+/**
+ * @author Graves
+ * @DESCRIPTION 是否是2的整数幂 - O(1)
+ * @create 2020/5/25
+ */
+public class IsPowerOf2 {
+
+    public static boolean isPowerOf2(int num){
+        return (num&num-1) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOf2(2));
+        System.out.println(isPowerOf2(8));
+        System.out.println(isPowerOf2(10));
+    }
+}
